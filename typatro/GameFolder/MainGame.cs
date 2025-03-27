83e5af8ad8b7c8bfc9531c39a987b25b0af36da3
Jan Filip <jafisik@gmail.com>
2025-03-27 22:54:26 +0100

    menu in its class

--- typatro/GameFolder/MainGame.cs
+++ typatro/GameFolder/MainGame.cs
@@ -2,12 +2,9 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 
 
 namespace typatro.GameFolder;
@@ -17,7 +14,7 @@ public class MainGame : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     SpriteFont font;
-    
+
     List<char> writtenText = new List<char>();
     string neededText = "";
     List<string> jsonStrings;
@@ -27,22 +24,14 @@ public class MainGame : Game
 
     enum GameState
     {
-        MENU,
-        OPTIONS,
-        PLAY
-    }
-    enum MenuSelect
-    {
         PLAY,
         OPTIONS,
-        EXIT
+        EXIT,
+        MENU
     }
-    private MenuSelect menuSelect = MenuSelect.PLAY;
-    bool menuNav = true;
+
     private GameState gameState = GameState.MENU;
     Menu menu;
-
-
 
     public MainGame()
     {
@@ -61,20 +50,24 @@ public class MainGame : Game
         Random rand = new Random();
         string jsonText = File.ReadAllText("Content/wordlist.json");
         jsonStrings = JsonSerializer.Deserialize<List<string>>(jsonText);
-        for(int i = 0; i < 20; i++){
-            neededText += jsonStrings[rand.Next(0,jsonStrings.Count)] + " ";
+        for (int i = 0; i < 20; i++)
+        {
+            neededText += jsonStrings[rand.Next(0, jsonStrings.Count)] + " ";
         }
-        neededText += jsonStrings[rand.Next(0,jsonStrings.Count)];
+        neededText += jsonStrings[rand.Next(0, jsonStrings.Count)];
 
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         font = Content.Load<SpriteFont>("Fonts/pixelFont");
         writer = new(_spriteBatch, font, diffIndexes, writtenText);
-        //menu = new Menu(_spriteBatch,font, GraphicsDevice, gameState);
+
+        Texture2D texture = new Texture2D(GraphicsDevice, 1, 1);
+        texture.SetData(new[] { Color.White });
+        menu = new Menu(_spriteBatch, font, texture);
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if(gameState == GameState.PLAY)
+        if (gameState == GameState.PLAY)
         {
             writer.ReadKeyboardInput(gameTime);
             writer.UpdateDiffIndexes(neededText);
@@ -87,82 +80,12 @@ public class MainGame : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
 
-        if(gameState == GameState.MENU)
+        if (gameState == GameState.MENU)
         {
-            Texture2D texture = new Texture2D(GraphicsDevice, 1, 1);
-            texture.SetData(new[] { Color.White });
-            Color menu1 = Color.LightGray, menu2 = Color.LightGray, menu3 = Color.LightGray;
-            KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Down))
-            {
-                if (menuNav)
-                {
-                    menuNav = false;
-                    if(menuSelect != MenuSelect.EXIT)
-                    {
-                        menuSelect++;
-                    }
-                    
-                }
-            }else if(state.IsKeyDown(Keys.Up))
-            {
-                if (menuNav)
-                {
-                    menuNav = false;
-                    if (menuSelect != MenuSelect.PLAY)
-                    {
-                        menuSelect--;
-                    }
-                }
-            }
-            if(state.IsKeyUp(Keys.Up) && state.IsKeyUp(Keys.Down))
-            {
-                menuNav = true;
-            }
-            switch (menuSelect)
-            {
-                case (MenuSelect.PLAY):
-                    menu1 = Color.Gray;
-                    menu2 = Color.LightGray;
-                    menu3 = Color.LightGray;
-                    if (state.IsKeyDown(Keys.Enter)) gameState = GameState.PLAY;
-                    break;
-                case (MenuSelect.OPTIONS):
-                    menu1 = Color.LightGray;
-                    menu2 = Color.Gray;
-                    menu3 = Color.LightGray;
-                    break;
-                case (MenuSelect.EXIT):
-                    menu1 = Color.LightGray;
-                    menu2 = Color.LightGray;
-                    menu3 = Color.Gray;
-                    if (state.IsKeyDown(Keys.Enter)) Exit();
-                    break;
-            }
-            
-            int menuLineSpacing = 100, topOffset = 80, line = 0;
-            int rectWidth = 400, rectHeight = 80;
-
-            _spriteBatch.Draw(texture, new Rectangle((_graphics.PreferredBackBufferWidth - rectWidth) / 2, topOffset + menuLineSpacing * line, rectWidth, rectHeight), menu1);
-            string menuText = "Play";
-            Vector2 textSize = font.MeasureString(menuText);
-            Vector2 textPos = new Vector2((_graphics.PreferredBackBufferWidth - textSize.X*2) / 2, 80 + rectHeight/2-textSize.Y/2 + menuLineSpacing * line++);
-            _spriteBatch.DrawString(font, menuText, textPos, Color.Black, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
-
-            _spriteBatch.Draw(texture, new Rectangle((_graphics.PreferredBackBufferWidth - rectWidth) / 2, 80 + menuLineSpacing * line, rectWidth, rectHeight), menu2);
-            menuText = "Options";
-            textSize = font.MeasureString(menuText);
-            textPos = new Vector2((_graphics.PreferredBackBufferWidth - textSize.X*2) / 2, 80 + rectHeight/2-textSize.Y/2 + menuLineSpacing * line++);
-            _spriteBatch.DrawString(font, menuText, textPos, Color.Black, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
-
-            _spriteBatch.Draw(texture, new Rectangle((_graphics.PreferredBackBufferWidth - rectWidth) / 2, 80 + menuLineSpacing * line, rectWidth, rectHeight), menu3);
-            menuText = "Exit";
-            textSize = font.MeasureString(menuText);
-            textPos = new Vector2((_graphics.PreferredBackBufferWidth - textSize.X*2) / 2, 80 + rectHeight/2-textSize.Y/2 + menuLineSpacing * line++);
-            _spriteBatch.DrawString(font, menuText, textPos, Color.Black, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
+            gameState = (GameState)menu.DrawMenu(_graphics);
         }
 
-        else if(gameState == GameState.PLAY)
+        else if (gameState == GameState.PLAY)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape)) gameState = GameState.MENU;
 
@@ -173,7 +96,17 @@ public class MainGame : Game
             //Calculate correct/wrong precentage
             writer.WriteText("Correct: " + (writtenText.Count > 0 ? ((1f - (diffIndexes.Count / (float)writtenText.Count)) * 100).ToString("0.00") + "%" : "0%"), Color.Black, 5);
         }
-       
+
+        else if (gameState == GameState.OPTIONS)
+        {
+
+        }
+
+        else if (gameState == GameState.EXIT)
+        {
+            Exit();
+        }
+
         _spriteBatch.End();
         base.Draw(gameTime);
     }
--- typatro/GameFolder/Menu.cs
+++ typatro/GameFolder/Menu.cs
@@ -1,26 +1,76 @@
-
-
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 
-namespace typatro.GameFolder{
+namespace typatro.GameFolder
+{
 
-    class Menu{
+    class Menu
+    {
         SpriteBatch _spriteBatch;
         SpriteFont font;
-        GraphicsDevice graphicsDevice;
-        GraphicsDeviceManager _graphics;
+        Texture2D texture;
+        int menuLineSpacing = 100, topOffset = 80, rectWidth = 400, rectHeight = 80;
+        enum MenuSelect
+        {
+            PLAY,
+            OPTIONS,
+            EXIT,
+            MENU
+        }
+        private MenuSelect menuSelect = MenuSelect.PLAY;
+        string[] menuTexts;
+        bool menuNav = true;
 
-        public Menu(SpriteBatch spriteBatch, SpriteFont font){
+        public Menu(SpriteBatch spriteBatch, SpriteFont font, Texture2D texture)
+        {
             this._spriteBatch = spriteBatch;
             this.font = font;
+            this.texture = texture;
+            int menuSelectLength = Enum.GetValues(typeof(MenuSelect)).Length - 1;
+            List<string> tempMenuTexts = new List<string>(menuSelectLength);
+            for (int menuItems = 0; menuItems < menuSelectLength; menuItems++)
+            {
+                tempMenuTexts.Add(((MenuSelect)menuItems).ToString().ToLower());
+            }
+            menuTexts = tempMenuTexts.ToArray();
         }
 
+        //To add new new menu item just add an item into enum MenuSelect and in the class MainGame add the same thing to enum GameState
+        public int DrawMenu(GraphicsDeviceManager graphicsDevice)
+        {
+            Color[] menuColors = new Color[menuTexts.Length];
+            KeyboardState state = Keyboard.GetState();
+            if (menuNav)
+            {
+                if (state.IsKeyDown(Keys.Down) && menuSelect != MenuSelect.EXIT) menuSelect++;
+                else if (state.IsKeyDown(Keys.Up) && menuSelect != MenuSelect.PLAY) menuSelect--;
+                menuNav = false;
+            }
 
-        public void DrawMenu(){
+            if (state.IsKeyUp(Keys.Up) && state.IsKeyUp(Keys.Down))
+            {
+                menuNav = true;
+            }
 
+            for (int menuIndex = 0; menuIndex < menuTexts.Length; menuIndex++)
+            {
+                if ((int)menuSelect == menuIndex) menuColors[menuIndex] = Color.Gray;
+                else menuColors[menuIndex] = Color.LightGray;
+            }
+            if (state.IsKeyDown(Keys.Enter)) return (int)menuSelect;
+
+            for (int line = 0; line < menuTexts.Length; line++)
+            {
+                _spriteBatch.Draw(texture, new Rectangle((graphicsDevice.PreferredBackBufferWidth - rectWidth) / 2, topOffset + menuLineSpacing * line, rectWidth, rectHeight), menuColors[line]);
+                Vector2 textSize = font.MeasureString(menuTexts[line]);
+                Vector2 textPos = new Vector2((graphicsDevice.PreferredBackBufferWidth - textSize.X * 2) / 2, 80 + rectHeight / 2 - textSize.Y / 2 + menuLineSpacing * line);
+                _spriteBatch.DrawString(font, menuTexts[line], textPos, Color.Black, 0f, Vector2.Zero, 2f, SpriteEffects.None, 0f);
+            }
+            return (int)MenuSelect.MENU;
         }
     }
 }
